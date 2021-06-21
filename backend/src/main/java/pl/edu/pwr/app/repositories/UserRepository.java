05059f47d51f3be6810b59672aa084d383c5c66b
kpucilowski248405 <248405@student.pwr.edu.pl>
2021-06-21 13:49:49 +0200

    add license

--- backend/src/main/java/pl/edu/pwr/app/repositories/UserRepository.java
+++ backend/src/main/java/pl/edu/pwr/app/repositories/UserRepository.java
@@ -1,3 +1,10 @@
+/* This source code is licensed under MIT License (the "License").
+   You may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   https://opensource.org/licenses/MIT
+
+ */
 package pl.edu.pwr.app.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
